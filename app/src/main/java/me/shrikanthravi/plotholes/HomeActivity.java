8fcf92747d9584963a3e776549985eadd483ea3f
shrikanth7698 <shrikanth7698@gmail.com>
2019-01-19 23:25:17 +0530

    Added TF model

--- app/src/main/java/me/shrikanthravi/plotholes/HomeActivity.java
+++ app/src/main/java/me/shrikanthravi/plotholes/HomeActivity.java
@@ -1,23 +1,37 @@
 package me.shrikanthravi.plotholes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.mapbox.mapboxsdk.MapmyIndia;
 import com.mmi.services.account.MapmyIndiaAccountManager;
 
+import butterknife.ButterKnife;
+import me.shrikanthravi.plotholes.services.SensorService;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        init();
+
+    }
+
+    public void init(){
+
+        ButterKnife.bind(this);
         MapmyIndiaAccountManager.getInstance().setRestAPIKey("bvyi1llc42hbcyx1u59wmzi4x2acxqtl");
         MapmyIndiaAccountManager.getInstance().setMapSDKKey("lr3xon9422rdzoputa3fmwuja4787z5r");
         MapmyIndiaAccountManager.getInstance().setAtlasGrantType("client_credentials");
         MapmyIndiaAccountManager.getInstance().setAtlasClientId("-T9_3PSz5MZyDVT9A6zVT1Ym8iUtEzQy2Lndg_7SvFM=");
         MapmyIndiaAccountManager.getInstance().setAtlasClientSecret("hk0REzhbnCc6sikl9yGPSCxry6Vydi8Us9JASA2Udcy_wc3uk_b2qA==");
         MapmyIndiaAccountManager.getInstance().setAtlasAPIVersion("1.3.11");
-        setContentView(R.layout.activity_home);
         MapmyIndia.getInstance(getApplicationContext());
+        setContentView(R.layout.activity_home);
+
+        //Start this service when in navigation mode
+        startService(new Intent(getApplicationContext(), SensorService.class));
     }
 }
--- app/src/main/java/me/shrikanthravi/plotholes/services/SensorService.java
+++ app/src/main/java/me/shrikanthravi/plotholes/services/SensorService.java
@@ -1,7 +1,10 @@
 package me.shrikanthravi.plotholes.services;
 
 import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,14 +12,26 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
+import android.widget.Toast;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SensorService extends Service implements SensorEventListener {
 
     public SensorService() {
     }
-
+    private static final int N_SAMPLES = 7;
+    private static List<Float> x = new ArrayList<>();
+    private static List<Float> y = new ArrayList<>();
+    private static List<Float> z = new ArrayList<>();
+    private TensorFlowClassifier classifier;
     private SensorManager mSensorManager;
     private Sensor mAccelerometer;
+    double lat,lng;
+    LatLngBroadcastReceiver latLngBroadcastReceiver;
+
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -30,18 +45,48 @@ public class SensorService extends Service implements SensorEventListener {
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+        classifier = new TensorFlowClassifier(getApplicationContext());
+        latLngBroadcastReceiver = new LatLngBroadcastReceiver();
+        final IntentFilter intentFilter = new IntentFilter("LatLngBroadcastReceiver");
+        LocalBroadcastManager.getInstance(this).registerReceiver(latLngBroadcastReceiver, intentFilter);
     }
 
 
     @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        final int type = sensorEvent.sensor.getType();
-        float x = sensorEvent.values[0];
-        float y = sensorEvent.values[1];
-        float z = sensorEvent.values[2];
-        sendBroadcast(x,y,z);
+    public void onSensorChanged(SensorEvent event) {
+        final int type = event.sensor.getType();
+        classifyData();
+        x.add(event.values[0]);
+        y.add(event.values[1]);
+        z.add(event.values[2]);
+
+
+
+
+        //sendBroadcast(x,y,z);
         //System.out.println("Accelerometer data\nx -> "+x+"\ny -> "+y+"\nz -> "+z);
 
+    }
+
+    private float[] results;
+
+    private void classifyData(){
+        if (x.size() == N_SAMPLES && y.size() == N_SAMPLES && z.size() == N_SAMPLES) {
+            List<Float> data = new ArrayList<>();
+            data.addAll(x);
+            data.addAll(y);
+            data.addAll(z);
+            results = classifier.predictProbabilities(toFloatArray(data));
+
+            String goodProb =  Float.toString(round(results[0], 3));
+            String badProb =  Float.toString(round(results[1], 3));
+            String resultText = "Good -> "+goodProb+"\nBad -> "+badProb;
+            Toast.makeText(getApplicationContext(),resultText,Toast.LENGTH_SHORT).show();
+
+            x.clear();
+            y.clear();
+            z.clear();
+        }
     }
 
     @Override
@@ -55,7 +100,7 @@ public class SensorService extends Service implements SensorEventListener {
         super.onDestroy();
     }
 
-    public void sendBroadcast(float x,float y,float z){
+    /*public void sendBroadcast(float x,float y,float z){
         Intent intent = new Intent("SensorBroadcastReceiver");
         Bundle bundle = new Bundle();
         bundle.putFloat("Acc_X",x);
@@ -63,5 +108,35 @@ public class SensorService extends Service implements SensorEventListener {
         bundle.putFloat("Acc_Z",z);
         intent.putExtras(bundle);
         LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+    }*/
+
+    public class LatLngBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            Bundle b = intent.getExtras();
+            lat = b.getFloat("lat");
+            lng = b.getFloat("lng");
+
+        }
     }
+
+    private float[] toFloatArray(List<Float> list) {
+        int i = 0;
+        float[] array = new float[list.size()];
+
+        for (Float f : list) {
+            array[i++] = (f != null ? f : Float.NaN);
+        }
+        return array;
+    }
+
+    private static float round(float d, int decimalPlace) {
+        BigDecimal bd = new BigDecimal(Float.toString(d));
+        bd = bd.setScale(decimalPlace, BigDecimal.ROUND_HALF_UP);
+        return bd.floatValue();
+    }
+
+
 }
