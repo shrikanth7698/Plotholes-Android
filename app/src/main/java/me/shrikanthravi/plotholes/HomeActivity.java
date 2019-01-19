3a4cbe8cf80673690cd1ce0d251a49d8d0f26ab5
Rishabh Singh <pingrishabh@gmail.com>
2019-01-19 21:01:21 +0530

    Added Vector SDK of MapMyIndia
    - Added .aar SDK
    - Added MapBox SDK
    - Added Retrofit
    - Added GSON
    - Added Google Play Location Services
    - Added relevant permissions
    - Added Dashboard constructors of MapMyIndia
    - Resolved API version error
    - Updated gradle version

--- app/src/main/java/me/shrikanthravi/plotholes/HomeActivity.java
+++ app/src/main/java/me/shrikanthravi/plotholes/HomeActivity.java
@@ -3,11 +3,21 @@ package me.shrikanthravi.plotholes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mapbox.mapboxsdk.MapmyIndia;
+import com.mmi.services.account.MapmyIndiaAccountManager;
+
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MapmyIndiaAccountManager.getInstance().setRestAPIKey("bvyi1llc42hbcyx1u59wmzi4x2acxqtl");
+        MapmyIndiaAccountManager.getInstance().setMapSDKKey("lr3xon9422rdzoputa3fmwuja4787z5r");
+        MapmyIndiaAccountManager.getInstance().setAtlasGrantType("client_credentials");
+        MapmyIndiaAccountManager.getInstance().setAtlasClientId("-T9_3PSz5MZyDVT9A6zVT1Ym8iUtEzQy2Lndg_7SvFM=");
+        MapmyIndiaAccountManager.getInstance().setAtlasClientSecret("hk0REzhbnCc6sikl9yGPSCxry6Vydi8Us9JASA2Udcy_wc3uk_b2qA==");
+        MapmyIndiaAccountManager.getInstance().setAtlasAPIVersion("1.3.11");
         setContentView(R.layout.activity_home);
+        MapmyIndia.getInstance(getApplicationContext());
     }
 }
